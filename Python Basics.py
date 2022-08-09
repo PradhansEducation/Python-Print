46627da0a3482575171af85f8861c3a3cd5ff33a
PradhansEducation <110892910+PradhansEducation@users.noreply.github.com>
2022-08-09 14:14:19 +0530

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,45 @@
+#Python Statements
+#single line statements
+num = 10
+print('num = ', num)
+
+num = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+print(num)
+
+#multiline statements
+num = 1 + 2 + 3 \
+    + 4 + 5 + 6 \
+        + 7 + 8 + 9 + 10
+print(num)
+
+message = "Hello learners, Python is a wonderful language to code in. " \
+    "This text is written over multiple lines to improve readability " \
+        "but it will be printed on single line in the screen."
+print(message)
+
+num = (1 + 2 + 3
++ 4 + 5 + 6
++ 7 + 8 + 9 + 10)
+print(num)
+
+message = ('Hello learners, Python is a wonderful language to code in. '
+'This text is written over multiple lines to improve readability '
+'but it will be printed on single line in the screen.')
+print(message)
+
+#multiple statements in a single line using semicolon
+a = 10; print(a); print('Hello World!')
+
+#single line comment
+# I am using a variable x
+x = 10
+print(x)
+
+#multiline comment
+'''
+multiline comment
+using variable x and y.
+'''
+x = 10
+y = 5
+print(x+y)
