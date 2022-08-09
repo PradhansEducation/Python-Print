6548dac44857f0264117eb25cbfd6afe169c5702
Pradhans Education <110892910+PradhansEducation@users.noreply.github.com>
2022-08-09 19:05:33 +0530

    Add files via upload

---
+++ Python Input.py
@@ -0,0 +1,33 @@
+name = input("What is your name >>>")
+print("Hello ", name)
+
+print("Enter your name ")
+x = input()
+y = input("age >>>") #age will be printed before input
+print('your name is', x, 'and your age is', y)
+
+print('Enter your Wish')
+wish = input()
+print("May your wish come true!")
+
+x = input('>>>')
+print(x)
+
+num = input('>>>')
+print(type(num))
+
+x = int(input("Enter an integer >>> "))
+print("You have entered", x)
+
+x = input('Enter an integer >>> ')
+y = int(x)
+print("You have entered", y)
+
+x = float(input('>>>'))
+print(x)
+print(type(x))
+
+import math
+print(math.sin(30))
+print(math.cos(10))
+print(math.pow(2, 3))
