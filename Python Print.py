42aed5b0fc728e415445f9663f2cce1aa1a8198d
PradhansEducation <110892910+PradhansEducation@users.noreply.github.com>
2022-08-09 13:48:36 +0530

    Add files via upload

---
+++ Python Print.py
@@ -0,0 +1,21 @@
+#printing hello world
+print('Hello World')
+print('Hey I am an upcoming coder!')
+print('I am going to step into the World of Programing!')
+print('I am loving this. print, print and print')
+
+#printing some mathematical operation
+x = 10
+y = 5
+print("sum of ", x, "and ", y, "is ", x+y)
+x = 10
+print(x)
+print('x')
+x = 'Hello World'
+print(x)
+print('x')
+
+#printing some funny codes
+print('a' + 'b')
+print('Fun'*3)
+print('Fun'+'Fun'+'Fun')
